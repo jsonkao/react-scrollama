9165f492bd39d25f98f3ee4b22780fa6fe298e17
Jason Kao <jason.kao@columbia.edu>
2020-04-28 13:56:41 -0400

    remove usage of react-jss

--- src/DebugOffset.js
+++ src/DebugOffset.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import injectSheet from 'react-jss';
 
-const styles = {
-  marker: {
-    position: 'fixed',
-    left: 0,
-    width: '100%',
-    height: 0,
-    borderTop: '2px dashed black',
-    zIndex: 9999,
-  },
-  offsetInfo: {
-    fontSize: '12px',
-    fontFamily: 'monospace',
-    margin: 0,
-    padding: 6,
-  },
+// Since these styles are the only styles in the library, I opted to use inline
+// styling to maintain dependency independence
+const markerStyles = {
+  position: 'fixed',
+  left: 0,
+  width: '100%',
+  height: 0,
+  borderTop: '2px dashed black',
+  zIndex: 9999,
+};
+const offsetTextStyles = {
+  fontSize: '12px',
+  fontFamily: 'monospace',
+  margin: 0,
+  padding: 6,
 };
 
-const DebugOffset = ({ classes, offsetMargin, offsetVal }) => (
-  <div className={classes.marker} style={{ top: offsetMargin }}>
-    <p className={classes.offsetInfo}>
+const DebugOffset = ({ offsetMargin, offsetVal }) => (
+  <div style={{ ...markerStyles, top: offsetMargin }}>
+    <p style={offsetTextStyles}>
       trigger: { offsetVal }
     </p>
   </div>
 );
 
-export default injectSheet(styles)(DebugOffset);
+export default DebugOffset;
