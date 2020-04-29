4e7618dd33b6da5b2029210e11995b98901b89ef
Jason Kao <jason.kao@columbia.edu>
2020-04-29 19:01:50 -0400

    cut down on devdependencies, updated rollup 2 and all rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-
-import pkg from './package.json'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import builtins from 'rollup-plugin-node-builtins';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
@@ -13,37 +11,29 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
-      sourcemap: true
+      sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
-  external: [ 'crypto' ],
   plugins: [
-    external(),
-    postcss({
-      modules: true
-    }),
-    url(),
+    peerDepsExternal(),
+    builtins(),
     babel({
       babelrc: false,
-      runtimeHelpers: true,
       presets: [
         ['@babel/preset-env', { modules: false }],
         '@babel/react',
       ],
       plugins: [
-        '@babel/plugin-transform-runtime',
         '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-proposal-function-bind',
       ],
       exclude: 'node_modules/**',
     }),
     resolve(),
-    commonjs()
-  ]
-}
+    commonjs(),
+  ],
+};
