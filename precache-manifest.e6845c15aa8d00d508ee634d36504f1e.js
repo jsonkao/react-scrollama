df914d86b487283ede50cdbd632a29392d91a8da
Jason Kao <jason.kao@columbia.edu>
2020-05-03 21:32:10 -0400

    Updates

---
+++ precache-manifest.e6845c15aa8d00d508ee634d36504f1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4430fda8a15bc2c03887ef02898b7fa1",
+    "url": "/react-scrollama/index.html"
+  },
+  {
+    "revision": "2df1d182118a365cff79",
+    "url": "/react-scrollama/static/js/2.df54d184.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "/react-scrollama/static/js/2.df54d184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a3db1a833f648b27f0",
+    "url": "/react-scrollama/static/js/main.57b59bc2.chunk.js"
+  },
+  {
+    "revision": "f1c6e1c9e95d89efd413",
+    "url": "/react-scrollama/static/js/runtime-main.336695f8.js"
+  }
+]);
