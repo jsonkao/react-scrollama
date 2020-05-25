5d708948a9bc92c5394f7954ccaa391632b50bb6
Jason Kao <jason.kao@columbia.edu>
2020-05-25 15:40:55 -0400

    Updates

---
+++ precache-manifest.a9132934af784eaaa1917a14a0a1c18f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29f7d0ae72a00223e2fe9830125d912",
+    "url": "/react-scrollama/index.html"
+  },
+  {
+    "revision": "8d97b38b8c882359f2de",
+    "url": "/react-scrollama/static/js/2.8dd8a381.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "/react-scrollama/static/js/2.8dd8a381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf64108cb5174cf3a7c",
+    "url": "/react-scrollama/static/js/main.89dd6680.chunk.js"
+  },
+  {
+    "revision": "f1c6e1c9e95d89efd413",
+    "url": "/react-scrollama/static/js/runtime-main.336695f8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-scrollama/precache-manifest.e6845c15aa8d00d508ee634d36504f1e.js"
+  "/react-scrollama/precache-manifest.a9132934af784eaaa1917a14a0a1c18f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-scrollama/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
