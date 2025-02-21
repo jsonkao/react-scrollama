61e358b04b26d42eab0e558227fa0e988f162c03
tuckergordon <gordon_tucker@bah.com>
2025-02-21 11:45:43 -0500

    Eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import reactPlugin from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginJs from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default [
+  pluginJs.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  jsxA11y.flatConfigs.recommended,
+  includeIgnoreFile(gitignorePath),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      sourceType: 'module',
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'import/no-named-as-default': 0,
+      'import/no-named-as-default-member': 0,
+      'import/first': 0,
+
+      'react/prop-types': [
+        2,
+        {
+          skipUndeclared: true,
+        },
+      ],
+
+      'arrow-parens': [2, 'as-needed'],
+
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['to'],
+          aspects: ['noHref', 'invalidHref', 'preferButton'],
+        },
+      ],
+    },
+  },
+];
